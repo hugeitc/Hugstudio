2b0fb089771c77c2046be91093ec2a1a87936793
hugeitc <nguyendanhhung@gmail.com>
2015-12-14 15:01:50 +0700

    first use git

---
+++ KanjiFlashCard/src/com/hugstudio/kanjiflashcard/MainActivity.java
@@ -0,0 +1,16 @@
+package com.hugstudio.kanjiflashcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	public MainActivity() {
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_main);
+	}
+}
